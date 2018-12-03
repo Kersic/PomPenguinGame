43f1158b1861872d87c6e94d8bf52ee666bba7b8
DESKTOP-DFU5HC7\Filip <horvatfilip7@gmail.com>
2018-12-03 18:19:39 +0100

    "prvi" test

---
+++ PomPenguinServer/test/moduls_test/penguin_test.js
@@ -0,0 +1,9 @@
+const mocha = require ("mocha");
+const assert = require("chai").assert;
+
+describe('test_pingvina',function(){
+   it('Test pingvina',function(){
+      let restult = 'Heloo';
+      assert.equal(restult,'Heloo');
+   });
+});
